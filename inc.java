d236d2fc95404a39f157b15ac567cd4bd2a13eaa
shjung <shei77@naver.com>
2024-07-10 18:34:49 +0900

    Create inc.java

---
+++ inc.java
@@ -0,0 +1,18 @@
+public class inc
+{
+ public static void main(String[] args)
+ {
+
+
+  int a=5, b=0, c=0;
+  b=a++;
+
+
+  System.out.println(a);
+  System.out.println(b);
+
+
+  c=++a;
+  System.out.println(c);
+ }
+}
