c932ce59b913bb01269fabd3e1fb20ba7e0f4b8d
shjung <shei77@naver.com>
2024-07-10 17:43:57 +0900

    Create ByteTest.java

---
+++ ByteTest.java
@@ -0,0 +1,17 @@
+/* profeel 2006 */
+
+public class ByteTest
+{
+ public static void main(String[] args)
+ {
+  char c = 'A';
+  char d = 65;
+  char e = '\u0042'; // B 출력
+
+ 
+
+  System.out.println("문자형 출력: " + c);
+  System.out.println("문자형 출력: " + d);
+  System.out.println("문자형 출력: " + e);
+ }
+}
