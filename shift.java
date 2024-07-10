1fcb91d922913271ccf4daa22a89115ea7d3dbc7
shjung <shei77@naver.com>
2024-07-10 18:35:08 +0900

    Create shift.java

---
+++ shift.java
@@ -0,0 +1,26 @@
+public class shift
+{
+ public static void main(String[] args)
+ {
+
+
+  int a=192 << 1;
+  System.out.println(a);
+
+ 
+
+  int b=192 << 1;
+
+  System.out.println(b);
+
+ 
+
+  int c=-192 >> 1;
+  System.out.println(c);
+
+ 
+
+  int d=-192 >>> 1;
+  System.out.println(d);
+ }
+}
