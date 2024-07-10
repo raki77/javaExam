659e76f2ab5724b4a08cff3567d07ae5cfac95fa
shjung <shei77@naver.com>
2024-07-10 18:33:14 +0900

    Create guguDan.java

---
+++ guguDan.java
@@ -0,0 +1,14 @@
+ 
+public class guguDan
+{
+ public static void main(String[] args)
+ {
+  for(int i=2; i<=9; i++) {
+   System.out.println(i + " Dan");
+   for(int j=1; j<=9; j++) {
+    System.out.println( i + "*" + j + "=" + (i*j));
+   }
+   System.out.println("");
+  }
+ }
+}
