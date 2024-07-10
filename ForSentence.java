29230832a57b731db76b748a584a446c40d10a55
shjung <shei77@naver.com>
2024-07-10 18:32:19 +0900

    Create ForSentence.java

---
+++ ForSentence.java
@@ -0,0 +1,13 @@
+class ForSentence {
+
+             public static void main (String args[]) {
+
+                          int i;
+                          System.out.println("For문 테스트 ");
+                         for (i=0; i < 100 ; i++) {
+
+                                       System.out.println(i + "번테스트");
+                     }
+                          System.out.println("For문 테스트끝");
+             }
+}
