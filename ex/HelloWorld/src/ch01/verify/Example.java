6021366f6ebde3d57c2970dfb91bd42274e75942
raki77 <rakisiss77@gmail.com>
2024-09-27 08:13:22 +0900

    111111111

---
+++ ex/HelloWorld/src/ch01/verify/Example.java
@@ -0,0 +1,10 @@
+package ch01.verify;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+	}
+
+}
