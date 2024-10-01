7748dc42b7830d796749acdcb25dc8285c8f5679
raki77 <rakisiss77@gmail.com>
2024-10-02 08:54:12 +0900

    241002-2

---
+++ ex/HelloWorld/src/pkg1/Test6.java
@@ -0,0 +1,26 @@
+package pkg1;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+	}
+
+}
+
+
+//6_13
+class Member {
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	public Member(String name, String id) {	
+		this.name = name;
+		this.id = id; 
+	}	 
+}
+
