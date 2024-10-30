d3ec932e986bfaef293f9d67493432d145467b9e
raki77 <rakisiss77@gmail.com>
2024-10-30 10:47:49 +0900

    241030-01

---
+++ spring01/helloworld2/src/main/java/com/shjung/helloworld2/TestController.java
@@ -0,0 +1,31 @@
+package com.shjung.helloworld2;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TestServlet", value = "/test")
+public class TestController extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
